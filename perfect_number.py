7db66ecf12db3661767b8c22ebcfa3d9b14d7b21
Juan Pablo <juan.perez.2019@upb.edu.co>
2019-03-30 16:52:50 -0500

    Décimo commit

--- perfect_number.py
+++ perfect_number.py
@@ -7,6 +7,10 @@ def perfect_number(n):
         print("The number you have typed is a perfect number.")
     else:
         print("The number you have typed is not a perfect number.")
+    if ((a==(n-3) or a==(n+3))):
+            print("The number you have typed isn´t an almost perfect number")
+    else:
+        print("The number you have typed is an almost perfect number")
 
 n=int(input("Number to evaluate: "))
 perfect_number(n)
