0e19c61b66e0810463686a1a6e38bbe517c51f58
PrivateGoose <47455200+PrivateGoose@users.noreply.github.com>
2019-03-30 10:47:59 -0500

    Mi cuarto commit

--- a_power_b.py
+++ a_power_b.py
@@ -11,7 +11,11 @@ def potencia(a,b):
 potencia(a,b)
 
 while True:
-  a=int(input(b))
-  elev=elev*a
-  if a==0:
-    break
+    a=int(input("Digite cualquier número si desea continuar, en caso contrario, digite 0 para acabar el programa: "))
+    if a==0:
+        print("Programa finalizado")
+        break
+    else:
+        a=int(input("Digite su número base: "))
+        b=int(input("Digite su número exponente: "))
+        print("Resultado:",potencia(a,b))
