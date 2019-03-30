0aec923c5b05dd115a92fef5216d8dd2731988f8
Juan Pablo <juan.perez.2019@upb.edu.co>
2019-03-30 15:55:26 -0500

    Octavo commit

--- Is_prime.py
+++ Is_prime.py
@@ -13,15 +13,18 @@ def is_prime(number):
             print("-1")
 
 counter = 0
+counterprime = 0
 
 while True:
     a=int(input("Type a number to check if it is a prime number, if you wish to proceed, press any number, if you do not want to proceed, press a number equal or less than 0: "))
     if a<0:
         print("Program finalized")
         print("The number of times you got a prime number is: {}".format(counter))
+        print("The quantity of primes number you got is also a prime number: {}".format(counterprime))
         break
     else:
         if a > 0:
             counter= counter + 1
+            counterprime=(counter%2!=0)
             number=int(input("Type your number: "))
             print(is_prime(number))
